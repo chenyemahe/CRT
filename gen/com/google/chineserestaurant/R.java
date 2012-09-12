ce95dfdc3743f7e617b55822620de64c85e36c03
Ye Chen <chenyemahe@gmail.com>
2012-09-12 17:51:39 -0500

    loginpage update1

--- gen/com/google/chineserestaurant/R.java
+++ gen/com/google/chineserestaurant/R.java
@@ -23,14 +23,19 @@ public final class R {
         public static final int launch_image2=0x7f020004;
         public static final int launch_image3=0x7f020005;
         public static final int launch_image4=0x7f020006;
+        public static final int login=0x7f020007;
     }
     public static final class id {
+        public static final int editText1=0x7f080002;
+        public static final int editText2=0x7f080004;
         public static final int imageView1=0x7f080000;
-        public static final int menu_settings=0x7f080001;
+        public static final int menu_settings=0x7f080005;
+        public static final int textView1=0x7f080001;
+        public static final int textView2=0x7f080003;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int launch_activity=0x7f030001;
+        public static final int launch_activity=0x7f030000;
+        public static final int login_activity=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
@@ -39,6 +44,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
+        public static final int null_string=0x7f050004;
         public static final int title_activity_main=0x7f050003;
     }
     public static final class style {
